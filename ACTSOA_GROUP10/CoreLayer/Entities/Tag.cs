422ec1f7fb0882ff26f569730f0cd275261c5d0d
Vo Minh Phuc <vominhphucqn21@gmail.com>
2025-02-20 15:28:21 +0700

    firstjkjkjk

---
+++ ACTSOA_GROUP10/CoreLayer/Entities/Tag.cs
@@ -0,0 +1,10 @@
+﻿namespace ACTSOA_GROUP10.CoreLayer.Entities
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<MovieSeriesTag> MovieSeriesTags { get; set; }
+
+    }
+}
